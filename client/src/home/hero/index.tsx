912c3524fff1e78ed9bf67cfdc8d387cef224264
Jason <79180247+Bobsters6666@users.noreply.github.com>
2023-07-13 13:54:00 +1200

    added hero and intro sections

--- client/src/home/hero/index.tsx
+++ client/src/home/hero/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import './index.css'
+import arrowDown from '../../assets/icons/arrow-down.svg'
 
-type Props = {}
-
-const Hero = (props: Props) => {
+const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+      <div>
+        <h1 className='h1'>JJD STUDIO</h1>
+        <h4>the best gaming studio in town, <br />lorem ipsum dorjt ipsum</h4>
+      </div>
+
+      <div className='learn-more'>
+        <a href="#intro"><img src={arrowDown} alt="" draggable="false"/></a>
+        <h4>Learn more</h4>
+      </div>
+    </div>
   )
 }
 
