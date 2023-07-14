8f662a8586ef9da303489ef292af2526b95028c7
Jason <79180247+Bobsters6666@users.noreply.github.com>
2023-07-14 22:15:00 +1200

    added message

--- client/src/home/message/index.tsx
+++ client/src/home/message/index.tsx
@@ -2,12 +2,17 @@ import './index.css'
 
 const Message = () => {
   return (
-    <div className='message desktop'>
-      <div>
-        <h2>Don't miss out on the biggest release of 2023</h2>
-        <button>Register your interest</button>
+    <>
+      <div className='message desktop'>
+        <div>
+          <h2>Don't miss out on the biggest release of 2023</h2>
+          <button>Register your interest</button>
+        </div>
       </div>
-    </div>
+
+      {/* Ellipses */}
+      <div className='ellipse-1 message-ellipse-1'></div>
+    </>
   )
 }
 
